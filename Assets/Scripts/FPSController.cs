4367f10619cd81a71dc3203a2aba5dfb8d074677
Alumno <john.ayudante@tecsup.edu.pe>
2025-12-04 12:57:26 -0500

    casi de la entrega

---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 150f;
+    public Transform cameraPivot;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Look();
+        Move();
+    }
+
+    void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        cameraPivot.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
